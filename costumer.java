edb7b48e5f2f7c8b4bf5376ffb87f2daba9a8112
Danyarkhdir <91218084+Danyarkhdir@users.noreply.github.com>
2021-10-05 11:24:16 +0300

    costumer class created

---
+++ costumer.java
@@ -0,0 +1,37 @@
+public class costumer {
+    private String name;
+    private long phoneNumber;
+    private String address; 
+    public  costumer() {
+        name="unknown";
+        phoneNumber=0000000000l;
+        address="no Address";
+    }
+    public  costumer(String name,long phoneNumber,String address) {
+        this.name=name;
+        this.phoneNumber=phoneNumber;
+        this.address=address;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getAddress() {
+        return address;
+    }
+    
+    public String toString() {
+        return "Costumer Name : "+name+"\nPhone Number : +964"+phoneNumber+"\naddress : "+address;
+    }
+}
