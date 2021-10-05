cf7574b0780152a52620f216e6295e7c9061c583
Danyarkhdir <91218084+Danyarkhdir@users.noreply.github.com>
2021-10-05 11:28:16 +0300

    main class updated
    objects of other class created and tested.

--- Main.java
+++ Main.java
@@ -1,4 +1,11 @@
 public class Main{
     public static void main(String[] args) {
+            costumer costumer1=new costumer("muhamad",07501234567,"Barzan-Erbil-Kurdistan");
+        items item1=new items(12, "Coca Cola", "Coca Company",5000);
+       order order1=new order(costumer1,item1,3);
+       bill bill1=new bill(order1);
+       System.out.println(bill1);
+
+      
     }
 }
