c58b89d08d7c222e87a3357603d5f16dc8c7def1
Danyarkhdir <ensw01119@student.su.edu.krd>
2021-10-26 09:01:57 +0300

    main

--- Main.java
+++ Main.java
@@ -1,4 +1,20 @@
 import java.util.*;
+
+import View.AdminView;
 public class Main{
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter USERNAME : ");
+        String username=sc.nextLine();
+        System.out.print("Enter PASSWORD : ");
+        String password=sc.nextLine();
+        if (!username.equals("inventory")||!password.equals("inventory")) {
+            System.out.println("username or password is incorrect");
+            return;
+        }
+        AdminView.login();
+
+
+    }
 
 }
