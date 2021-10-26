df0dd7ed2859ec0d9a7c3fff06d92a4c55dd9c4c
Danyarkhdir <ensw01119@student.su.edu.krd>
2021-10-26 08:56:44 +0300

    some deletion

--- Main.java
+++ Main.java
@@ -1,17 +1,4 @@
 import java.util.*;
 public class Main{
-    public static void main(String[] args) {
-    Admin admin1=new Admin("arman","132435");
-    Order order1=new Order();
-    admin1.addItem(new Items(13, "USB", 10000));
-    admin1.addCostumer(new Costumer(11, "ahmad", "122343", "bile"));
-    admin1.addOrder(order1);
-    order1.addItemToOrder(new Items(1, "Pc", 200000));
-    order1.addItemToOrder(new Items(13,"headset",25000));
-    System.out.println(admin1);
-    System.out.println(order1);
-    
-    
 
-    }
 }
